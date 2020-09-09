b99a17bdf826d9c31a50048cd55da977abbf2cd6
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-09-09 01:39:35 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -45,6 +45,7 @@ class MusicLibraryController
     puts "Please enter the name of an artist:"
     input = gets.chomp
     songs_by_artist = Song.all.select {|song| song.artist.name == input}
+    if songs_by_artist
   end
   
 end
