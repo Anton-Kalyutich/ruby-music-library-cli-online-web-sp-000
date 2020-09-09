d7634ff5839dc3466ded4ddc5d151815fa52bc89
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-09-09 02:02:44 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -63,8 +63,7 @@ class MusicLibraryController
   end
   
   def play_song
-    
-    list_songs
+    song = list_songs
     puts "Which song number would you like to play?"
     input = gets.chomp.to_i
     if input >=1 && input <= Song.all.length
