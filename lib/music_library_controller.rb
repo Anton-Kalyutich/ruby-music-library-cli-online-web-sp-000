2464b08bb1748a73d59afb6be9bd725af200d4bb
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-09-09 01:25:43 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -44,7 +44,7 @@ class MusicLibraryController
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
     input = gets.chomp
-    songs_by_artist
+    songs_by_artist = 
   end
   
 end
