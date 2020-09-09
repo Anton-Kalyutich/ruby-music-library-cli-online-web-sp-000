bcb9a1d305406f5089652d528f9ac141c07a2f88
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-09-09 02:21:49 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -24,6 +24,9 @@ class MusicLibraryController
           list_artists
         when "list genres"
           list_genres
+        when "list artist"
+          list_songs_by_artist
+        when
     end
   end
     
