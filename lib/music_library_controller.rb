6cc933c33da7655f7281ad94df00f132de30de1b
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-09-09 01:36:13 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -20,7 +20,7 @@ class MusicLibraryController
     end
   end
     
-  def list_songs(songs = Song.all)
+  def list_songs
     songs = songs.sort_by {|song| song.name}
     songs.each_with_index do |song, index|
       puts "#{index+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
