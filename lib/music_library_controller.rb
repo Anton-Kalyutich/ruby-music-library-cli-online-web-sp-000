5d20779c46f1931856d9b3cec048c31e4d923a22
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-09-09 01:46:14 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -44,7 +44,7 @@ class MusicLibraryController
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
     input = gets.chomp
-    songs = Song.all_by_artist_name(input}
+    songs = Song.all_by_artist_name(input)
     songs.each_with_index do |song, index|
       puts "#{index+1}. #{song.name} - #{song.genre}"
     end
